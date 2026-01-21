273c31d2f17d6d2feee05673fc30793e16ad6f92
Josiah Martin <130996468+JustJoey3434@users.noreply.github.com>
2026-01-20 20:22:00 -0500

    Updated IO Interface and Sim Class

--- src/main/java/frc/robot/subsystems/DriveIO.java
+++ src/main/java/frc/robot/subsystems/DriveIO.java
@@ -1,18 +1,25 @@
-package frc.robot.subsystems;
+package frc.robot.subsystems; //
 
 public interface DriveIO {
     public static class DriveInterfaceInputs {
         public double leftVelocity = 0.0;
         public double rightVelocity = 0.0;
 
-        public double leftPositionRad = 0.0;
-        public double rightPositionRad = 0.0;
+        public double leftPosition = 0.0;
+        public double rightPosition = 0.0;
 
         public double leftAppliedVoltage = 0.0;
         public double rightAppliedVoltage = 0.0;
+
+        public double[] leftAppliedCurrent;
+        public double[] rightAppliedCurrent;
     }
 
-    public default void updateInputs(DriveInterfaceInputs inputs) {
+    public default void update(double dt) {
+
+    }
+
+    public default void updateInputs(DriveInterfaceInputs leftInputs, DriveInterfaceInputs rightInputs) {
 
     }
 
@@ -21,6 +28,10 @@ public interface DriveIO {
     }
 
     public default void setVoltage(double leftVoltage, double rightVoltage) {
+
+    }
+
+    public default void setCurrent(double leftCurrent, double rightCurrent) {
 
     }
 }
