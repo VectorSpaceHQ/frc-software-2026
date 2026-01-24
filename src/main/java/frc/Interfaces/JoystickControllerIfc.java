6be3c0f1fb885a86f81f21e73037d51cde2d4a17
SonOfGojira511 <253821719+SonOfGojira511@users.noreply.github.com>
2026-01-24 11:20:27 -0500

    almost finished joystick interface, not enough triggers, commented out

--- src/main/java/frc/Interfaces/JoystickControllerIfc.java
+++ src/main/java/frc/Interfaces/JoystickControllerIfc.java
@@ -1,16 +1,48 @@
+//COMMANDJOYSTICK DOESN'T HAVE ENOUGH TRIGGERS TO INTERFACE CONTROLLER IFC
+
 // package frc.Interfaces;
 
 // import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+// import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-// public class JoystickControllerIfc extends ControllerIfc {
+// public class JoystickControllerIfc implements ControllerIfc {
 
+//   CommandJoystick exampleJoystick;
+    
 //     public JoystickControllerIfc(int port) {
-//         final CommandJoystick exampleJoystick = new CommandJoystick(port);
-//         swerveX = exampleJoystick.getX();
-//         swerveY = exampleJoystick.getY();
-//         rotate = exampleJoystick.getTwist();
-//         shoot = exampleJoystick.top();
+        
+//         exampleJoystick = new CommandJoystick(port);
 //     }
     
+//     public double getX() {
+        
+//         return exampleJoystick.getX();
+//     };
+      
+//     public double getY() {
 
+//         return exampleJoystick.getY();
+//     }
+
+//     public double getTwist(){
+
+//         return exampleJoystick.getTwist();
+//     }
+
+//     public Trigger runShooter(){
+
+//         return new Trigger(false);
+//     }
+
+//     public Trigger runIntake(){
+
+//         return exampleJoystick.top();
+//     }
+
+//     public Trigger stopIntake(){
+
+//         return exampleJoystick.trigger();
+//     }
+
+ 
 // }
