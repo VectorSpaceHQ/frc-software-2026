7348f2f6da6c8922d5e490c4fb737fce3d595cb6
CJ <198853284+CJ2258@users.noreply.github.com>
2026-01-31 11:10:55 -0500

    chnaged motor to pos

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -40,7 +40,7 @@ public class ShooterSubsystem {
         double t_targetRadsPerSec = Units.rotationsPerMinuteToRadiansPerSecond(t_targetRPM);
         t_volts = feedforward.calculate(t_targetRadsPerSec);
 
-        double getTwist = -.2;
+        double getTwist = .2;
         double b_targetRPM = (getTwist * MAX_RPM);
         double b_targetRadsPerSec = Units.rotationsPerMinuteToRadiansPerSecond(b_targetRPM);
         b_volts = feedforward.calculate(b_targetRadsPerSec);
