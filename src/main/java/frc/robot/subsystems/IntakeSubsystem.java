6523bfe9e667481bd829c49b3f479732e4b5172b
SonOfGojira511 <253821719+SonOfGojira511@users.noreply.github.com>
2026-02-03 20:29:53 -0500

    did some housekeeping in the intake class

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -23,8 +23,8 @@ import frc.robot.components.motor.MotorIO.MotorIOInputs;
 
 
 public class IntakeSubsystem extends SubsystemBase implements Sendable {
-    private final MotorIO Feed_motor1;
-    private final MotorIO Feed_motor2;
+    private final MotorIO Feed1;
+    private final MotorIO Feed2;
     private final double MAX_RPM = 6000; // 628.32 rads/s
 
     private final double velocity_MOTOR = Units.rotationsPerMinuteToRadiansPerSecond(509.3); // 53.33 rads/s
@@ -34,8 +34,8 @@ public class IntakeSubsystem extends SubsystemBase implements Sendable {
     // private final ControllerIfc m_driverController;
     // private final ControllerIfc m_operatorController;
 
-    private final MotorIOInputs FeedMotor1Inputs;
-    private final MotorIOInputs FeedMotor2Inputs;
+    private final MotorIOInputs Feed1Inputs;
+    private final MotorIOInputs Feed2Inputs;
 
     private boolean Intakestatus;
     private SimpleMotorFeedforward Feed1_feedforward;
@@ -53,24 +53,24 @@ public class IntakeSubsystem extends SubsystemBase implements Sendable {
     private double kd = 0;
     private double ki = 0;
 
-    double l_motorspeed;
-    double r_motorspeed;
+    double Feed1_motorspeed;
+    double Feed2_motorspeed;
     double Feed1_volts;
     double Feed2_volts;
 
-    double Motor1_RPM = 0.2 * MAX_RPM;
-    double Motor2_RPM = -(Motor1_RPM);
-    double Motor1_realRPM = 0;
-    double Motor2_realRPM = 0;
+    double Feed1_RPM = 0.2 * MAX_RPM;
+    double Feed2_RPM = -(Feed1_RPM);
+    double Feed1_realRPM = 0;
+    double Feed2_realRPM = 0;
 
     public IntakeSubsystem() {
-        Feed_motor1 = new MotorIOKraken(19);
-        Feed_motor2 = new MotorIOKraken(20);
-        FeedMotor1Inputs = new MotorIOInputs();
-        FeedMotor2Inputs = new MotorIOInputs();
+        Feed1 = new MotorIOKraken(19); //change canID later
+        Feed2 = new MotorIOKraken(20); //change canID later
+        Feed1Inputs = new MotorIOInputs();
+        Feed2Inputs = new MotorIOInputs();
         Feed1_feedforward = new SimpleMotorFeedforward(ks, kv);
         Feed2_feedforward = new SimpleMotorFeedforward(ks, kv);
-        Feed1_pid = new PIDController(kp, ki, kd);
+        Feed1_pid = new PIDController(kp, ki, kd);   // TODO decideif we need separate pids (ask henry)
         Feed1_pid.setTolerance(k_INTAKE_TOLERANCE_RPS);
         Intakestatus = false;
         SmartDashboard.putData("Intake", this);
@@ -78,14 +78,14 @@ public class IntakeSubsystem extends SubsystemBase implements Sendable {
 
     public void calculate() {
 
-        double t_targetRadsPerSec = Units.rotationsPerMinuteToRadiansPerSecond(Motor1_RPM);
-        double b_targetRadsPerSec = Units.rotationsPerMinuteToRadiansPerSecond(Motor2_RPM);
+        double Feed1_targetRadsPerSec = Units.rotationsPerMinuteToRadiansPerSecond(Feed1_RPM);
+        double Feed2_targetRadsPerSec = Units.rotationsPerMinuteToRadiansPerSecond(Feed2_RPM);
 
-        Feed1_volts = MathUtil.clamp(Feed1_feedforward.calculate(t_targetRadsPerSec)
-                + Feed1_pid.calculate(FeedMotor1Inputs.velocityRadPerSec, t_targetRadsPerSec), -12.0, 12.0);
+        Feed1_volts = MathUtil.clamp(Feed1_feedforward.calculate(Feed1_targetRadsPerSec)
+                + Feed1_pid.calculate(Feed1Inputs.velocityRadPerSec, Feed1_targetRadsPerSec), -12.0, 12.0);
                 
-        Feed2_volts = MathUtil.clamp(Feed2_feedforward.calculate(b_targetRadsPerSec)
-                + Feed1_pid.calculate(FeedMotor2Inputs.velocityRadPerSec, b_targetRadsPerSec), -12.0, 12.0);
+        Feed2_volts = MathUtil.clamp(Feed2_feedforward.calculate(Feed2_targetRadsPerSec)
+                + Feed1_pid.calculate(Feed2Inputs.velocityRadPerSec, Feed2_targetRadsPerSec), -12.0, 12.0);
 
     }
 
@@ -97,20 +97,20 @@ public class IntakeSubsystem extends SubsystemBase implements Sendable {
 
     @Override
     public void periodic() { // Update inputs, calculate, then set voltages every loop
-        Feed_motor1.updateInputs(FeedMotor1Inputs);
-        Feed_motor2.updateInputs(FeedMotor2Inputs);
+        Feed1.updateInputs(Feed1Inputs);
+        Feed2.updateInputs(Feed2Inputs);
 
-        Motor1_realRPM = Units.radiansPerSecondToRotationsPerMinute(FeedMotor1Inputs.velocityRadPerSec);
-        Motor2_realRPM = Units.radiansPerSecondToRotationsPerMinute(FeedMotor2Inputs.velocityRadPerSec);
+        Feed1_realRPM = Units.radiansPerSecondToRotationsPerMinute(Feed1Inputs.velocityRadPerSec);
+        Feed2_realRPM = Units.radiansPerSecondToRotationsPerMinute(Feed2Inputs.velocityRadPerSec);
 
         if (!Intakestatus) {
             calculate();
 
-            Feed_motor1.setVoltage(Feed1_volts);
-            Feed_motor2.setVoltage(Feed2_volts);
+            Feed1.setVoltage(Feed1_volts);
+            Feed2.setVoltage(Feed2_volts);
         } else {
-            Feed_motor1.setVoltage(0);
-            Feed_motor2.setVoltage(0);
+            Feed1.setVoltage(0);
+            Feed2.setVoltage(0);
         }
     }
 
@@ -118,49 +118,49 @@ public class IntakeSubsystem extends SubsystemBase implements Sendable {
     public void initSendable(SendableBuilder builder) {
         System.out.println("Intake init sendable called");
         builder.setSmartDashboardType("Intake Controller");
-        builder.addDoubleProperty("LeftRPM", this::getl_RPM, this::setl_RPM);
-        builder.addDoubleProperty("RightRPM", this::getr_RPM, this::setr_RPM);
-        builder.addDoubleProperty("Left Volts", this::getl_volts, null);
-        builder.addDoubleProperty("Right Volts", this::getr_volts, null);
-        builder.addDoubleProperty("Left Real RPM", this::getl_realRPM, null);
-        builder.addDoubleProperty("Right Real RPM", this::getr_realRPM, null);
+        builder.addDoubleProperty("Feed1RPM", this::getFeed1_RPM, this::setFeed1_RPM);
+        builder.addDoubleProperty("Feed2RPM", this::getFeed2_RPM, this::setFeed2_RPM);
+        builder.addDoubleProperty("Feed1 Volts", this::getFeed1_volts, null);
+        builder.addDoubleProperty("Feed2 Volts", this::getFeed2_volts, null);
+        builder.addDoubleProperty("Feed1 Real RPM", this::getFeed1_realRPM, null);
+        builder.addDoubleProperty("Feed2 Real RPM", this::getFeed2_realRPM, null);
     }
 
-    public double getl_RPM() {
-        return Motor1_RPM;
+    public double getFeed1_RPM() {
+        return Feed1_RPM;
     }
 
-    public void setl_RPM(double l_RPM) {
-        if (l_RPM > MAX_RPM) {
-            l_RPM = MAX_RPM;
+    public void setFeed1_RPM(double Feed1_RPM) {
+        if (Feed1_RPM > MAX_RPM) {
+            Feed1_RPM = MAX_RPM;
         }
-        this.Motor1_RPM = l_RPM;
+        this.Feed1_RPM = Feed1_RPM;
     }
 
-    public double getr_RPM() {
-        return Motor2_RPM;
+    public double getFeed2_RPM() {
+        return Feed2_RPM;
     }
 
-    public void setr_RPM(double r_RPM) {
-        if (r_RPM > MAX_RPM) {
-            r_RPM = MAX_RPM;
+    public void setFeed2_RPM(double Feed2_RPM) {
+        if (Feed2_RPM > MAX_RPM) {
+            Feed2_RPM = MAX_RPM;
         }
-        this.Motor2_RPM = r_RPM;
+        this.Feed2_RPM = Feed2_RPM;
     }
 
-    public double getl_volts() {
+    public double getFeed1_volts() {
         return Feed1_volts;
     }
 
-    public double getr_volts() {
+    public double getFeed2_volts() {
         return Feed2_volts;
     }
 
-    public double getl_realRPM() {
-        return Motor1_realRPM;
+    public double getFeed1_realRPM() {
+        return Feed1_realRPM;
     }
 
-    public double getr_realRPM() {
-        return Motor2_realRPM;
+    public double getFeed2_realRPM() {
+        return Feed2_realRPM;
     }
 }
