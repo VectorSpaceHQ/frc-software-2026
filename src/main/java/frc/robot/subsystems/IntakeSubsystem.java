3070035c3955c76fd4e9dcd99dc0d4291b53c4ed
SonOfGojira511 <253821719+SonOfGojira511@users.noreply.github.com>
2026-02-14 10:50:55 -0500

    fixed intake subsystem ( needs tuning)

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -25,9 +25,9 @@ public class IntakeSubsystem extends SubsystemBase implements Sendable {
     private final ArmFeedforward pivotFeedforward;
 
     public IntakeSubsystem() {
-        IntakeRollers1 = new PID(new MotorIOSparkMax(11),6000, 12, 0.25, 0.0015, 0.01, 0);
-        IntakeRollers2 = new PID(new MotorIOSparkMax(12),-6000, 12, 0.25, 0.0015, 0.01, 0);
-        pivotMotorPid = new PID(new MotorIOSparkMax(13),-6000, 12, 0.25, 0.0015, 0.01, 0);
+        IntakeRollers1 = new PID("IntakeRollers1",new MotorIOSparkMax(11),6000.0, 12.0, 0.25, 0.0015, 0.01, 0.0);
+        IntakeRollers2 = new PID("IntakeRollers2" ,new MotorIOSparkMax(12),-6000, 12, 0.25, 0.0015, 0.01, 0);
+        pivotMotorPid = new PID("PivotMotor" ,new MotorIOSparkMax(13),-6000, 12, 0.25, 0.0015, 0.01, 0);
         pivotFeedforward = new ArmFeedforward(0, 1.75, 1.95); // TODO: need to calibrate
   // Feed1Inputs = new MotorIOInputs();
   // Feed2Inputs = new MotorIOInputs();
@@ -66,8 +66,8 @@ public class IntakeSubsystem extends SubsystemBase implements Sendable {
         builder.setSmartDashboardType("Intake Controller");
         builder.addBooleanProperty("Shooter Status", this::getIntakestatus, null);
         builder.addBooleanProperty("Last Shooter Status", this::getLastIntakestatus, null);
-        IntakeRollers1.getPIDStatus(builder);
-        IntakeRollers2.getPIDStatus(builder);
+        IntakeRollers1.initSendable(builder);
+        IntakeRollers2.initSendable(builder);
     }
 
 }
