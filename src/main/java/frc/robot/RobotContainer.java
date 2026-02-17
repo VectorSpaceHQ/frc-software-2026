9967871d59dd6f758ba17840c51f27d794ed699a
Josiah Martin <justjoeyyt9@gmail.com>
2026-02-17 18:50:44 -0500

    Latest changes from enoch branch

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -31,48 +31,56 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
-
 import static edu.wpi.first.units.Units.Volts;
 
 import edu.wpi.first.math.util.Units;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  //subsystems:
-  private final ShooterSubsysConfig ShooterSSConfig = new ShooterSubsysConfig(true, SHOOTER_SUBSYSTEM);
+  // subsystems:
+  // private final ShooterSubsysConfig ShooterSSConfig = new
+  // ShooterSubsysConfig(true, SHOOTER_SUBSYSTEM);
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final ShooterSubsystem m_ShooterSubsystem = new ShooterSubsystem(ShooterSSConfig);
-  private final IntakeSubsystem m_IntakeSubsystem = new IntakeSubsystem();
+  // private final ShooterSubsystem m_ShooterSubsystem = new
+  // ShooterSubsystem(ShooterSSConfig);
+  // private final IntakeSubsystem m_IntakeSubsystem = new IntakeSubsystem();
   private final SwerveSubsystem drivebase = new SwerveSubsystem();
 
-  //create 2 instances of our new controller interface:
+  // create 2 instances of our new controller interface:
   private final ControllerIfc m_driverController = new XboxControllerIfc(OperatorConstants.controllerPort1);
-  private final ControllerIfc m_operatorController = new XboxControllerIfc(OperatorConstants.controllerPort2);
+  // private final ControllerIfc m_operatorController = new
+  // XboxControllerIfc(OperatorConstants.controllerPort2);
 
   SwerveInputStream driveAngularVelocity = SwerveInputStream.of(drivebase.getSwerveDrive(),
-                                                                () -> m_driverController.getY() * -1,
-                                                                () -> m_driverController.getX() * -1)
-                                                            .withControllerRotationAxis(m_driverController::getTwist)
-                                                            .deadband(OperatorConstants.DEADBAND)
-                                                            .scaleTranslation(0.8)
-                                                            .allianceRelativeControl(false);
+      () -> m_driverController.getY() * -1,
+      () -> m_driverController.getX() * -1)
+      .withControllerRotationAxis(m_driverController::getTwist)
+      .deadband(OperatorConstants.DEADBAND)
+      .scaleTranslation(0.8)
+      .allianceRelativeControl(false);
   /**
-   * Clone's the angular velocity input stream and converts it to a fieldRelative input stream.
+   * Clone's the angular velocity input stream and converts it to a fieldRelative
+   * input stream.
    */
   SwerveInputStream driveDirectAngle = driveAngularVelocity.copy().withControllerHeadingAxis(m_driverController::getX,
-                                                                                             m_driverController::getY)
-                                                           .headingWhile(true);
+      m_driverController::getY)
+      .headingWhile(true);
 
-  Command driveFieldOrientedDirectAngle      = drivebase.driveFieldOriented(driveDirectAngle);
+  Command driveFieldOrientedDirectAngle = drivebase.driveFieldOriented(driveDirectAngle);
   Command driveFieldOrientedAnglularVelocity = drivebase.driveFieldOriented(driveAngularVelocity);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
 
     // Configure the trigger bindings
@@ -81,92 +89,104 @@ public class RobotContainer {
   }
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    //new Trigger(m_exampleSubsystem::exampleCondition)
-        //.onTrue(new ExampleCommand(m_exampleSubsystem));
+    // new Trigger(m_exampleSubsystem::exampleCondition)
+    // .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-     //new Trigger(m_Shooter::commandShooter)
-         //.onTrue(new ControllerCommand(m_driverController));
-    
+    // new Trigger(m_Shooter::commandShooter)
+    // .onTrue(new ControllerCommand(m_driverController));
+
     // new Trigger(m_driverController::commandShooter)
-    //     .onTrue(new ControllerCommand(m_driverController));      
+    // .onTrue(new ControllerCommand(m_driverController));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is
+    // pressed,
     // cancelling on release.
 
-    //TODO: Temporary start motor (Move to operator controller?)
+    // TODO: Temporary start motor (Move to operator controller?)
     // m_driverController.runIntake().onTrue(
-    //     new InstantCommand( () -> 
-    //     m_motor.setVoltage(6.0))
+    // new InstantCommand( () ->
+    // m_motor.setVoltage(6.0))
     // );
-    
+
     // Temporary stop motor
     // m_driverController.stopIntake().onTrue(
-    //     new InstantCommand( () -> 
-    //     m_motor.stop())
+    // new InstantCommand( () ->
+    // m_motor.stop())
     // );
 
-    //TODO: Should this be on the operator controller?
-   /*  m_driverController.runShooter().whileTrue(
-      new RunCommand( () -> {
-        double trigger = m_driverController.controlMotorSpeed();
-        double targetRPM = (trigger * MAX_RPM);
-        double targetRadsPerSec = Units.rotationsPerMinuteToRadiansPerSecond(targetRPM);
-        double volts = feedforward.calculate(targetRadsPerSec);
-        m_motor.setVoltage(volts);
-      }).withTimeout(3) 
-    );*/
+    // TODO: Should this be on the operator controller?
+    /*
+     * m_driverController.runShooter().whileTrue(
+     * new RunCommand( () -> {
+     * double trigger = m_driverController.controlMotorSpeed();
+     * double targetRPM = (trigger * MAX_RPM);
+     * double targetRadsPerSec =
+     * Units.rotationsPerMinuteToRadiansPerSecond(targetRPM);
+     * double volts = feedforward.calculate(targetRadsPerSec);
+     * m_motor.setVoltage(volts);
+     * }).withTimeout(3)
+     * );
+     */
 
-    //TODO: Replace onchange when class is futher developed (and move to operator controller?)
-    /*m_operatorController.startShooter().onTrue(
-      new InstantCommand( () -> 
-        m_ShooterSubsystem.toggleShoot())
-    
-    );
-      //   m_driverController.runIntake().onTrue(
-      // new InstantCommand( () -> 
-      //   m_IntakeSubsystem.toggleIntake())
-    
+    // TODO: Replace onchange when class is futher developed (and move to operator
+    // controller?)
+    /*
+     * m_operatorController.startShooter().onTrue(
+     * new InstantCommand( () ->
+     * m_ShooterSubsystem.toggleShoot())
+     * 
+     * );
+     * // m_driverController.runIntake().onTrue(
+     * // new InstantCommand( () ->
+     * // m_IntakeSubsystem.toggleIntake())
+     * 
+     * // );
+     * 
+     * // NEO (names of controller commands unimportant for this case / temporary)
+     * m_driverController.runIntake().whileTrue(
+     * m_ShooterSubsystem.sysIdNeoQuasistatic(SysIdRoutine.Direction.kForward)
+     * );
+     * 
+     * m_driverController.runShooter().whileTrue(
+     * m_ShooterSubsystem.sysIdNeoQuasistatic(SysIdRoutine.Direction.kReverse)
+     * );
+     * 
+     * m_driverController.stopIntake().whileTrue(
+     * m_ShooterSubsystem.sysIdNeoDynamic(SysIdRoutine.Direction.kForward)
+     * );
+     * 
+     * m_driverController.runClimb().whileTrue(
+     * m_ShooterSubsystem.sysIdNeoDynamic(SysIdRoutine.Direction.kReverse)
+     * );
+     * 
+     * 
+     * }
+     * 
+     * 
+     * 
+     * );
+     */
+
+    // TODO: Is this supposed to be on the operator controller?
+    // m_driverController.runIntake().onTrue(
+    // new InstantCommand( () ->
+    // m_IntakeSubsystem.toggleIntake())
     // );
-    
-    // NEO (names of controller commands unimportant for this case / temporary)
-m_driverController.runIntake().whileTrue(
-    m_ShooterSubsystem.sysIdNeoQuasistatic(SysIdRoutine.Direction.kForward)
-);
-
-m_driverController.runShooter().whileTrue(
-    m_ShooterSubsystem.sysIdNeoQuasistatic(SysIdRoutine.Direction.kReverse)
-);
-
-m_driverController.stopIntake().whileTrue(
-    m_ShooterSubsystem.sysIdNeoDynamic(SysIdRoutine.Direction.kForward)
-);
-
-m_driverController.runClimb().whileTrue(
-    m_ShooterSubsystem.sysIdNeoDynamic(SysIdRoutine.Direction.kReverse)
-);
-
-
-  }
-
-
-  
-    );*/
-
-    //TODO: Is this supposed to be on the operator controller?
-    //m_driverController.runIntake().onTrue(
-    //  new InstantCommand( () -> 
-    //  m_IntakeSubsystem.toggleIntake())
-    //);
   }
 
   /**
