c27337008e28cb2ad2827be2914e3e34c38f9285
Josiah Martin <justjoeyyt9@gmail.com>
2026-02-19 18:24:45 -0500

    Updated Gyro class

--- src/main/java/frc/robot/components/imu/Gyro.java
+++ src/main/java/frc/robot/components/imu/Gyro.java
@@ -3,33 +3,43 @@ package frc.robot.components.imu;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 import com.ctre.phoenix6.BaseStatusSignal;
-import com.ctre.phoenix6.StatusCode;
+
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.Pigeon2Configuration;
 import com.ctre.phoenix6.hardware.Pigeon2;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
+import frc.robot.components.imu.GyroIO.GyroIOInputs;
 
 public class Gyro implements GyroIO {
   // The gyro sensor
-  private Pigeon2 m_gyro = null;
-  private final StatusSignal<Angle> yaw = m_gyro.getYaw();
-  private final StatusSignal<AngularVelocity> yawVelocity = m_gyro.getAngularVelocityZWorld();
+
+  private final Pigeon2 m_gyro;
+
+  private final StatusSignal<Angle> yaw;
+  private final StatusSignal<AngularVelocity> yawVelocity;
 
   public Gyro() {
-    m_gyro = new Pigeon2(0, "rio"); // Make sure device ID and bus name are correct (add variable?)
+
+    m_gyro = new Pigeon2(0, "rio");
+
     m_gyro.getConfigurator().apply(new Pigeon2Configuration());
-    m_gyro.getConfigurator().setYaw(0.0); // Zero's yaw at startup?
+    m_gyro.setYaw(0.0);
+
+    yaw = m_gyro.getYaw();
+    yawVelocity = m_gyro.getAngularVelocityZWorld();
   }
 
   @Override
   public void updateInputs(GyroIOInputs inputs) {
-    inputs.connected = BaseStatusSignal.refreshAll(yaw, yawVelocity).equals(StatusCode.OK); // I assume if status code
-                                                                                            // is OK, then the signal is
-                                                                                            // valid and the gyro is
-                                                                                            // connected.
-    inputs.rotation2d = Rotation2d.fromDegrees(yaw.getValueAsDouble());
-    inputs.rotation3d = m_gyro.getRotation3d(); // Is this really necessary?
+
+    BaseStatusSignal.refreshAll(yaw, yawVelocity);
+
+    inputs.connected = true;
+
+    inputs.rotation2d = Rotation2d.fromDegrees(
+        yaw.getValueAsDouble());
+
     inputs.yawVelocityRadPerSec = Math.toRadians(yawVelocity.getValueAsDouble());
   }
 
