32046443924efe7be6e6fa694a902314728a7959
Josiah Martin <justjoeyyt9@gmail.com>
2026-02-18 19:59:34 -0500

    Updated gyro interfaces

--- src/main/java/frc/robot/components/imu/Gyro.java
+++ src/main/java/frc/robot/components/imu/Gyro.java
@@ -1,29 +1,37 @@
 package frc.robot.components.imu;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.Pigeon2Configuration;
 import com.ctre.phoenix6.hardware.Pigeon2;
-import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
 
 public class Gyro implements GyroIO {
   // The gyro sensor
   private Pigeon2 m_gyro = null;
+  private final StatusSignal<Angle> yaw = m_gyro.getYaw();
+  private final StatusSignal<AngularVelocity> yawVelocity = m_gyro.getAngularVelocityZWorld();
 
   public Gyro() {
-    m_gyro = new Pigeon2(0, "rio");
-  }
-
-
-  @Override
-  public Rotation2d getRotation2d() {
-    return m_gyro.getRotation2d();
+    m_gyro = new Pigeon2(0, "rio"); // Make sure device ID and bus name are correct (add variable?)
+    m_gyro.getConfigurator().apply(new Pigeon2Configuration());
+    m_gyro.getConfigurator().setYaw(0.0); // Zero's yaw at startup?
   }
 
   @Override
-  public Rotation3d getRotation3d() {
-    return m_gyro.getRotation3d();
+  public void updateInputs(GyroIOInputs inputs) {
+    inputs.connected = BaseStatusSignal.refreshAll(yaw, yawVelocity).equals(StatusCode.OK); // I assume if status code
+                                                                                            // is OK, then the signal is
+                                                                                            // valid and the gyro is
+                                                                                            // connected.
+    inputs.rotation2d = Rotation2d.fromDegrees(yaw.getValueAsDouble());
+    inputs.rotation3d = m_gyro.getRotation3d(); // Is this really necessary?
+    inputs.yawVelocityRadPerSec = Math.toRadians(yawVelocity.getValueAsDouble());
   }
-
- 
 
   @Override
   public void reset() {
@@ -32,9 +40,7 @@ public class Gyro implements GyroIO {
 
   @Override
   public void DisplayIMUData() { // Lazy
-    System.out.println("Gyro Rotation2d: " + getRotation2d());
-    System.out.println("Gyro Rotation3d: " + getRotation3d());
-    System.out.println("Gyro Yaw: " + m_gyro.getYaw());
-    
+    System.out.println("Gyro Yaw: " + yaw.getValueAsDouble());
+
   }
 }
--- src/main/java/frc/robot/components/imu/GyroIO.java
+++ src/main/java/frc/robot/components/imu/GyroIO.java
@@ -1,11 +1,25 @@
 package frc.robot.components.imu;
+
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 
 public interface GyroIO {
-  public Rotation2d getRotation2d();
-  public Rotation3d getRotation3d();
-  // public double getRate();
-  public void reset();
-  public void DisplayIMUData();
+  public class GyroIOInputs {
+    public boolean connected = false;
+    public Rotation2d rotation2d = new Rotation2d(); // Yaw position
+    public Rotation3d rotation3d = new Rotation3d();
+    public double yawVelocityRadPerSec = 0.0;
+  }
+
+  // public double getRate(); Add getVelocityZWorld() from core pigeon2 instead?
+  // Idk
+  
+  public default void reset() {
+  }
+
+  public default void DisplayIMUData() {
+  }
+
+  public default void updateInputs(GyroIOInputs inputs) {
+  }
 }
