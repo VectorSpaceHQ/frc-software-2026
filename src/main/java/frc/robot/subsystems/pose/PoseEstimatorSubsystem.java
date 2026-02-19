32d89af1bc159b8e3bf26259daef613773b8058b
Josiah Martin <justjoeyyt9@gmail.com>
2026-02-19 18:22:31 -0500

    Added temporary pose estimator subsystem

---
+++ src/main/java/frc/robot/subsystems/pose/PoseEstimatorSubsystem.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems.pose;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PoseEstimatorSubsystem extends SubsystemBase {
+        // private final GyroIO m_gyro;
+        // private final SwerveSubsystem m_swerveSubsystem;
+        // private final SwerveDrivePoseEstimator poseEstimator;
+    
+        public PoseEstimatorSubsystem() {
+            // m_gyro = new GyroIOPigeon2(0);
+            // m_swerveSubsystem = new SwerveSubsystem();
+            // poseEstimator = new SwerveDrivePoseEstimator(m_gyro, m_swerveSubsystem);
+        }
+    
+        @Override
+        public void periodic() {
+            // poseEstimator.update();
+        }
+}
