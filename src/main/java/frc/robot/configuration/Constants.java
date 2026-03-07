34f442542453f7b63aba16df6da3bd6bd7aa369a
Vector Space <adam@vector-space.org>
2026-03-07 15:05:34 -0500

    fixed camera offsets and imu inversion. Odometry is now pretty accurate.

--- src/main/java/frc/robot/configuration/Constants.java
+++ src/main/java/frc/robot/configuration/Constants.java
@@ -165,12 +165,12 @@ public final class Constants {
 
     // Constants for the Transformation3d objects for the camera and robot
     public static final double TRANSLATION_X = -0.1778; // Meters forward from the robot center
-    public static final double TRANSLATION_Y = 0.3302; // Meters to the left from the robot center
+    public static final double TRANSLATION_Y = -0.3302; // Meters to the left from the robot center
     public static final double TRANSLATION_Z = 0.53345; // Meters above the robot center
 
     public static final double ROTATION_X = Math.toRadians(-90); // 90 degree rotation around the X-axis CCW
     public static final double ROTATION_Y = Math.toRadians(-20); // Rotate 20 degrees upward
-    public static final double ROTATION_Z = Math.toRadians(90); // 90 degree rotation around the Z-axis CW
+    public static final double ROTATION_Z = Math.toRadians(-90); // 90 degree rotation around the Z-axis CW
     public static final Vector<N3> VISION_ST_DEVS = VecBuilder.fill(0.7, 0.7, Math.toRadians(15)); // Meters, Meters,
                                                                                                    // Degrees
 
