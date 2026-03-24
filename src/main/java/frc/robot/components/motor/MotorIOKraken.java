6a27d14e9d21701dfb775292064a98e2ba5626c4
Funkyfox2 <gnochmaestro@gmail.com>
2026-03-24 11:34:25 -0400

    Removing unused import

--- src/main/java/frc/robot/components/motor/MotorIOKraken.java
+++ src/main/java/frc/robot/components/motor/MotorIOKraken.java
@@ -1,7 +1,6 @@
 package frc.robot.components.motor;
 
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.NeutralOut;
