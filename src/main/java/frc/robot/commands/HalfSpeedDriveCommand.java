9e5cb3aa1927d17d1ab530efa1a63c82a7b6587d
Funkyfox2 <gnochmaestro@gmail.com>
2026-03-25 01:49:02 -0400

    added logging for half speed drive with "Half Speed Drive" on smart dashboard

--- src/main/java/frc/robot/commands/HalfSpeedDriveCommand.java
+++ src/main/java/frc/robot/commands/HalfSpeedDriveCommand.java
@@ -1,17 +1,8 @@
 package frc.robot.commands;
 
-import org.littletonrobotics.junction.Logger;
-
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.units.Units;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.configuration.Constants.ShooterConstants;
-import frc.robot.configuration.Constants.VisionConstants;
+
 import frc.robot.subsystems.drive.SwerveSubsystem;
 
 public class HalfSpeedDriveCommand extends Command {
@@ -20,18 +11,16 @@ public class HalfSpeedDriveCommand extends Command {
 
     public HalfSpeedDriveCommand(SwerveSubsystem swerve) {
         this.swerve = swerve;
-
         addRequirements(swerve);
     }
 
     @Override
     public void initialize() {
-        SmartDashboard.putString("AimTowardsHub/Status", "Initialized");
+        SmartDashboard.putString("Half Speed Drive", "Enabled");
     }
 
     @Override
     public void execute() {
-        SmartDashboard.putString("AimTowardsHub/Status", "Executing");
         // Drive using the dedicated half speed stream in Swerve. Feed raw speeds directly
         // to the drive method.
         swerve.getSwerveDrive().drive(swerve.getHalfSpeedStream().get());
@@ -41,7 +30,7 @@ public class HalfSpeedDriveCommand extends Command {
 
     @Override
     public void end(boolean interrupted) {
-
+        SmartDashboard.putString("Half Speed Drive", "Disabled");
     }
 
 }
