480ffa0748ebeac63ce4ad184fc07aa873fc9627
Funkyfox2 <gnochmaestro@gmail.com>
2026-03-24 20:05:29 -0400

    half speed modifier on true set to 0.5 instead of 2

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -172,7 +172,7 @@ public class RobotContainer {
     // Do not require the swerve subsystem for these InstantCommands so they don't
     // interrupt longer-running drive/aim commands that also require swerve.
     m_driverController.halfSpeedModifier().onTrue(
-            new InstantCommand(() -> m_swerveSubsystem.setTranslationScaling(2))
+            new InstantCommand(() -> m_swerveSubsystem.setTranslationScaling(0.5))
         ).onFalse(
             new InstantCommand(() -> m_swerveSubsystem.setTranslationScaling(1))
         );           
